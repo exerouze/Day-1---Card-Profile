46be84d343ef8eea9754a525d12f749deeb0dc4b
exerouze <jafarabdullah586@gmail.com>
2024-01-10 17:58:02 +0700

    make responsive css

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,63 +1,64 @@
 const projects = [];
 
 function addProject(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const projectName = document.getElementById("project-name").value;
-    const startDate = document.getElementById("start-date").value;
-    const endDate = document.getElementById("end-date").value;
-    const description = document.getElementById("project-desc").value;
-    let upImage = document.getElementById("project-image").files;
-    upImage = URL.createObjectURL(upImage[0]);
-    const checkedTech = document.querySelectorAll('input[name="tech"]:checked');
+  const projectName = document.getElementById("project-name").value;
+  const startDate = document.getElementById("start-date").value;
+  const endDate = document.getElementById("end-date").value;
+  const description = document.getElementById("project-desc").value;
+  let upImage = document.getElementById("project-image").files;
+  upImage = URL.createObjectURL(upImage[0]);
+  const checkedTech = document.querySelectorAll('input[name="tech"]:checked');
 
-    const techValues = Array.from(checkedTech).map(tech => tech.value);
+  const techValues = Array.from(checkedTech).map((tech) => tech.value);
 
-    const project = {
-        projectName,
-        startDate,
-        endDate,
-        description,
-        upImage,
-        techValues,
-    };
+  const project = {
+    projectName,
+    startDate,
+    endDate,
+    description,
+    upImage,
+    techValues,
+  };
 
-    projects.unshift(project);
-    renderProject();
+  projects.unshift(project);
+  renderProject();
 
-    console.log('project', project);
+  console.log("project", project);
 }
 
 function renderProject() {
-    let html = '';
+  let html = "";
 
-    for (let index = 0; index < projects.length; index++) {
-        let icon = '';
+  for (let index = 0; index < projects.length; index++) {
+    let icon = "";
 
-        for (let i = 0; i < projects[index].techValues.length; i++) {
-            const tech = projects[index].techValues[i];
+    for (let i = 0; i < projects[index].techValues.length; i++) {
+      const tech = projects[index].techValues[i];
 
-            if (tech === "reactjs") {
-                icon += `<i class="fa-brands fa-react"></i>`;
-            }
-            if (tech === "nodejs") {
-                icon += `<i class="fa-brands fa-node-js"></i>`;
-            }
-            if (tech === "python") {
-                icon += `<i class="fa-brands fa-python"></i>`;
-            }
-            if (tech === "golang") {
-                icon += `<i class="fa-brands fa-golang"></i>`;
-            }
-           
-        }
+      if (tech === "reactjs") {
+        icon += `<i class="fa-brands fa-react"></i>`;
+      }
+      if (tech === "nodejs") {
+        icon += `<i class="fa-brands fa-node-js"></i>`;
+      }
+      if (tech === "python") {
+        icon += `<i class="fa-brands fa-python"></i>`;
+      }
+      if (tech === "golang") {
+        icon += `<i class="fa-brands fa-golang"></i>`;
+      }
+    }
 
-        // Hitung total bulan
-        const startDate = new Date(projects[index].startDate);
-        const endDate = new Date(projects[index].endDate);
-        const totalMonths = (endDate.getFullYear() - startDate.getFullYear()) * 12 + (endDate.getMonth() - startDate.getMonth());
+    // Hitung total bulan
+    const startDate = new Date(projects[index].startDate);
+    const endDate = new Date(projects[index].endDate);
+    const totalMonths =
+      (endDate.getFullYear() - startDate.getFullYear()) * 12 +
+      (endDate.getMonth() - startDate.getMonth());
 
-        html += `
+    html += `
             <div class="project-item">
                 <div class="project-image">
                     <img src="${projects[index].upImage}" alt="Project Image">
@@ -83,11 +84,9 @@ function renderProject() {
                     </div>
                 </div>
             </div>`;
-    }
+  }
 
-    document.getElementById("project-list").innerHTML = html;
+  document.getElementById("project-list").innerHTML = html;
 }
 
-
 renderProject();
-
